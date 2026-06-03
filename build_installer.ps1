39c9e066951dbfb0cd40dcf128781f9c9bc5f956
PAULO HENRIQUE VIANA DA SILVA <paulo.viana@centralit.com.br>
2026-06-03 17:04:53 -0300

    novo icone

--- build_installer.ps1
+++ build_installer.ps1
@@ -8,7 +8,7 @@ dotnet publish -c Release -r win-x64 --self-contained false -o publish
 
 Write-Host "3. Gerando o instalador com Velopack..."
 # O parametro -u define o nome da pasta no AppData, -v e a versao do app
-vpk pack -u PokeCollection -v 1.0.0 -p publish -e PokeCollection.exe
+vpk pack -u PokeCollection -v 1.0.0 -p publish -e PokeCollection.exe -i "C:\Users\Paulo Viana\OneDrive\Área de Trabalho\icon_exe.ico"
 
 Write-Host ""
 Write-Host "====================================================="
